6ec8cb08362b1a15bde8e02a4026cf473e0539c4
Julianrc15 <jarc2802@gmail.com>
2022-03-25 20:42:42 -0500

    funciones py

---
+++ funcion.py
@@ -0,0 +1,7 @@
+numero1 = int(input("digite numero 1"))
+numero2 = int(input("digite numero 2"))
+
+
+print(f'la suma es {numero1+numero2}')
+
+
---
+++ funcion2.py
@@ -0,0 +1,7 @@
+#creando una funcion py
+
+def sumar (numero1,numero2):
+    suma=numero1+numero2
+    return (suma)
+    
+print(sumar(10,10))
